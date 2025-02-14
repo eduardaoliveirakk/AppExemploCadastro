ed7b5d89999dc311eae12184aa8a960e03070c02
2022102020050 <2022102020050@JIPA-LABIN03-03>
2025-02-14 16:06:55 -0400

    Adicionar arquivos de projeto.

---
+++ AppExemploCadastro/Formulario/FormConsultaComb.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace AppExemploCadastro.Formulario
+{
+    partial class FormConsultaComb
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCpf = new System.Windows.Forms.TextBox();
+            this.txtRegistroGeral = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.bt_Atualizar = new System.Windows.Forms.Button();
+            this.bt_deletar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(38, 154);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(475, 26);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCpf.Location = new System.Drawing.Point(38, 224);
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(187, 26);
+            this.txtCpf.TabIndex = 1;
+            // 
+            // txtRegistroGeral
+            // 
+            this.txtRegistroGeral.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRegistroGeral.Location = new System.Drawing.Point(38, 299);
+            this.txtRegistroGeral.Name = "txtRegistroGeral";
+            this.txtRegistroGeral.Size = new System.Drawing.Size(187, 26);
+            this.txtRegistroGeral.TabIndex = 2;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(38, 377);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(187, 26);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "NOME:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "CPF";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 280);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "RG:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(35, 358);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "E-MAIL:";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(543, 42);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "CONSULTA DE PESSOA";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(69, 86);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(398, 28);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(69, 67);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(398, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "PESQUISAR NOME";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bt_Atualizar
+            // 
+            this.bt_Atualizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.bt_Atualizar.ForeColor = System.Drawing.Color.White;
+            this.bt_Atualizar.Location = new System.Drawing.Point(351, 345);
+            this.bt_Atualizar.Name = "bt_Atualizar";
+            this.bt_Atualizar.Size = new System.Drawing.Size(189, 23);
+            this.bt_Atualizar.TabIndex = 13;
+            this.bt_Atualizar.Text = "ATUALIZAR REGISTRO";
+            this.bt_Atualizar.UseVisualStyleBackColor = false;
+            this.bt_Atualizar.Click += new System.EventHandler(this.bt_Atualizar_Click);
+            // 
+            // bt_deletar
+            // 
+            this.bt_deletar.BackColor = System.Drawing.Color.Red;
+            this.bt_deletar.ForeColor = System.Drawing.Color.White;
+            this.bt_deletar.Location = new System.Drawing.Point(351, 374);
+            this.bt_deletar.Name = "bt_deletar";
+            this.bt_deletar.Size = new System.Drawing.Size(189, 29);
+            this.bt_deletar.TabIndex = 14;
+            this.bt_deletar.Text = "DELETAR";
+            this.bt_deletar.UseVisualStyleBackColor = false;
+            this.bt_deletar.Click += new System.EventHandler(this.bt_deletar_Click);
+            // 
+            // FormConsultaComb
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(567, 432);
+            this.Controls.Add(this.bt_deletar);
+            this.Controls.Add(this.bt_Atualizar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtRegistroGeral);
+            this.Controls.Add(this.txtCpf);
+            this.Controls.Add(this.txtNome);
+            this.Name = "FormConsultaComb";
+            this.Text = "FormConsultaComb";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCpf;
+        private System.Windows.Forms.TextBox txtRegistroGeral;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button bt_Atualizar;
+        private System.Windows.Forms.Button bt_deletar;
+    }
+}
---
+++ AppExemploCadastro/Formulario/FormConsultaComb.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AppExemploCadastro.Contexto;
+using AppExemploCadastro.Models;
+
+namespace AppExemploCadastro.Formulario
+{
+    public partial class FormConsultaComb : Form
+    {
+        int contExc = 0;
+        List<Pessoa> ListaPessoas = new List<Pessoa>();
+        public FormConsultaComb()
+        {
+            InitializeComponent();
+            PessoaContext context = new PessoaContext();//prepararou a conexão
+            ListaPessoas = context.ListarPessoas(); //conectou e buscou no banco
+            comboBox1.DataSource = ListaPessoas.ToList();
+            comboBox1.DisplayMember = "Nome";
+            comboBox1.SelectedIndex = -1;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var linhaSelec = comboBox1.SelectedIndex;
+            if (linhaSelec > -1 && contExc > 0)
+            {
+                var pessoaSelec = ListaPessoas[linhaSelec];
+                txtNome.Text = pessoaSelec.Nome;
+                txtCpf.Text = pessoaSelec.Cpf;
+                txtRegistroGeral.Text = pessoaSelec.RegistroGeral;
+                txtEmail.Text = pessoaSelec.Email;
+            }
+            contExc++;
+        }
+
+        /*
+        private void bt_atualizar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var linhaSelec = comboBox1.SelectedIndex;
+
+            if(linhaSelec > -1 && contExc > 0)
+            {
+                var pessoaSelec = ListaPessoas[linhaSelec];
+                pessoaSelec.Nome = txtNome.Text;
+                pessoaSelec.Cpf = txtCpf.Text;
+                pessoaSelec.Email = txtEmail.Text;
+                pessoaSelec.RegistroGeral = txtRegistroGeral.Text;
+
+
+                PessoaContext context = new PessoaContext();
+                context.AtualizarPessoa(pessoaSelec);
+                MessageBox.Show("ID: " + pessoaSelec.Id + "ATUALIZADO COM SUCESSO!", "2°A inf", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtNome.Clear();
+                txtEmail.Clear();
+                txtCpf.Clear();
+                txtRegistroGeral.Clear();
+
+                txtNome.Select();
+                contExc = 0;
+                ListaPessoas = context.ListarPessoas();
+                comboBox1.DataSource = ListaPessoas.ToList();
+                comboBox1.SelectedIndex = -1;
+
+            }
+
+
+        }
+*/
+        private void bt_deletar_Click(object sender, EventArgs e)
+        {
+            txtNome.Clear();
+            txtEmail.Clear();
+            txtCpf.Clear();
+            txtRegistroGeral.Clear();
+
+        }
+
+        private void bt_Atualizar_Click(object sender, EventArgs e)
+        {
+            var linhaSelec = comboBox1.SelectedIndex;
+
+            if (linhaSelec > -1 && contExc > 0)
+            {
+                var pessoaSelec = ListaPessoas[linhaSelec];
+                pessoaSelec.Nome = txtNome.Text;
+                pessoaSelec.Cpf = txtCpf.Text;
+                pessoaSelec.Email = txtEmail.Text;
+                pessoaSelec.RegistroGeral = txtRegistroGeral.Text;
+
+
+                PessoaContext context = new PessoaContext();
+                context.AtualizarPessoa(pessoaSelec);
+                MessageBox.Show("ID: " + pessoaSelec.Id + "ATUALIZADO COM SUCESSO!", "2°A inf", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtNome.Clear();
+                txtEmail.Clear();
+                txtCpf.Clear();
+                txtRegistroGeral.Clear();
+
+                txtNome.Select();
+                contExc = 0;
+                ListaPessoas = context.ListarPessoas();
+                comboBox1.DataSource = ListaPessoas.ToList();
+                comboBox1.SelectedIndex = -1;
+
+            }
+
+        }
+    }
+}
+
